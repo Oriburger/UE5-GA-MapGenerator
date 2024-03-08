a0a8f249dcb8329dfeaa45762c15f9749148d4d7
Jaeho Lee <uss425@naver.com>
2024-03-08 21:58:01 +0900

    초기 커밋

---
+++ Source/AnotherWorldStreet/AnotherWorldStreetGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AnotherWorldStreetGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANOTHERWORLDSTREET_API AAnotherWorldStreetGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
