c511f5c604d99a14ccbc141b54cdc15be6695b11
Jaeho Lee <uss425@naver.com>
2024-03-09 17:18:44 +0900

    [feat] 기본 기능 - 게임 시작 / 종료   - 초기화 / 시작 / 종료 이벤트 (델리게이트)   - 기본 UI 포함 - 플레이어 조작   - 이동, 점프, 조준, 공격   - 기본 애니메이션   - 무기 / 발사체 이벤트

--- Source/AnotherWorldStreet/AnotherWorldStreetGameModeBase.cpp
+++ Source/AnotherWorldStreet/AnotherWorldStreetGameModeBase.cpp
@@ -3,3 +3,13 @@
 
 #include "AnotherWorldStreetGameModeBase.h"
 
+void AAnotherWorldStreetGameModeBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	OnInitializeEndDelegate.AddDynamic(this, &AAnotherWorldStreetGameModeBase::OnInitializeEnded);
+}
+
+void AAnotherWorldStreetGameModeBase::OnInitializeEnded_Implementation(bool Result, FVector StartLocation)
+{
+}
--- Source/AnotherWorldStreet/AnotherWorldStreetGameModeBase.h
+++ Source/AnotherWorldStreet/AnotherWorldStreetGameModeBase.h
@@ -1,4 +1,4 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+ // Copyright Epic Games, Inc. All Rights Reserved.
 
 #pragma once
 
@@ -9,9 +9,40 @@
 /**
  * 
  */
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FDelegateAfterInit, bool, Result, FVector, StartLocation);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FDelegateGameFinish, float, TotalTime);
+
 UCLASS()
 class ANOTHERWORLDSTREET_API AAnotherWorldStreetGameModeBase : public AGameModeBase
 {
 	GENERATED_BODY()
-	
+
+//====== Delegate ==================
+public:
+	virtual void BeginPlay() override;
+
+//====== Delegate ==================
+public:
+	UPROPERTY(BlueprintAssignable, VisibleAnywhere, BlueprintCallable, Category = "Event")
+		FDelegateAfterInit OnInitializeEndDelegate;
+
+	UPROPERTY(BlueprintAssignable, VisibleAnywhere, BlueprintCallable, Category = "Event")
+		FDelegateGameFinish OnGameFinishedDelegate;
+
+	UFUNCTION(BlueprintNativeEvent)
+		void OnInitializeEnded(bool Result, FVector StartLocation);
+
+//====== Game Function ==================
+protected:
+	//Initialize map with data
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void InitializeGame();
+
+	//Start game after initializing game
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void StartGame();
+
+	//Finish game
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable)
+		void FinishGame();
 };
