e3e1f1fa70e31836345faa57babfb2f448376a60
hwangyuho <wnts134312@gmail.com>
2024-04-10 22:46:14 +0900

    [feat] Bezier Curve 이용한 초기모집단 Generator - GenControlPoint()함수 구현 - GenBezierCurve()함수 구현   - 조절점 2개 - MapGeneratorBase.h 수정   - StaticMeshList(Category = "Map Setting")

--- Source/AnotherWorldStreet/private/InitialMapGeneratorComponent.cpp
+++ Source/AnotherWorldStreet/private/InitialMapGeneratorComponent.cpp
@@ -2,6 +2,9 @@
 
 
 #include "../public/InitialMapGeneratorComponent.h"
+#include "../public/MapGeneratorBase.h"
+#include "Math/UnrealMathUtility.h"
+#include "Kismet/KismetMathLibrary.h"
 
 // Sets default values for this component's properties
 UInitialMapGeneratorComponent::UInitialMapGeneratorComponent()
@@ -10,13 +13,21 @@ UInitialMapGeneratorComponent::UInitialMapGeneratorComponent()
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
 
-	// ...
+
+	// ..
+	AMapGeneratorBase MapGeneratorBase;
+	StartLoc = MapGeneratorBase.StartLocation;
+	EndLoc = MapGeneratorBase.EndLocation;
+
+	checkf(IsValid(MapGeneratorBase.StaticMeshList[0]), TEXT("StaticMesh 탐색에 실패했습니다."));
+
 }
 
 // Called when the game starts
 void UInitialMapGeneratorComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
 }
 
 // Called every frame
@@ -27,6 +38,81 @@ void UInitialMapGeneratorComponent::TickComponent(float DeltaTime, ELevelTick Ti
 
 void UInitialMapGeneratorComponent::Generate_Implementation()
 {
-	//BP Native Event
-	//필요시에만 네이티브 코드를 추가하세요
+	AMapGeneratorBase MapGeneratorBase;
+
+	//UKismetMathLibrary::MakeTransform()
+	/*
+	for (FVector& CurvePoint : CurvePoints)
+	{
+
+	}
+
+	PlatformInfoResult.PlatformStaticMesh = MapGeneratorBase.StaticMeshList[0];
+	PlatformInfoResult.PlatformTransform = 
+
+	LastGenerateResult.PlatformInfoList.Add(PlatformInfoResult)
+	*/
+
+}
+
+FVector UInitialMapGeneratorComponent::GenControlPoint()
+{
+	FVector ControlPoint;
+
+	float Rand_X = FMath::FRandRange(5000.0f, 10000.0f);
+	float Rand_Y = FMath::FRandRange(5000.0f, 10000.0f);
+	float Rand_Z = FMath::FRandRange(1000.0f, 1500.0f);
+
+	ControlPoint.X = Rand_X;
+	ControlPoint.Y = Rand_Y;
+	ControlPoint.Z = Rand_Z;
+
+	return ControlPoint;
+}
+
+void UInitialMapGeneratorComponent::GenBezierCurve(FVector Start, FVector End, int32 GenPointNum, int32 ControlPointNum)
+{
+	FVector ResultCurve;
+
+	for (int32 i = 0; i < ControlPointNum; i++)
+	{
+		ControlPoints[i] = GenControlPoint();
+	}
+
+	//Control Point가 한개일때
+	/*
+	if(ControlPointNum == 1) 
+	{
+
+	}
+	*/
+
+	//Control Point가 한개일때
+	if(ControlPointNum == 2) 
+	{
+		for (int32 j = 0; j < GenPointNum; j++) {
+			double t = j / (GenPointNum - 1);
+			double oneMinusT = 1.0 - t;
+			double tSquared = t * t;
+			double oneMinusTSquared = oneMinusT * oneMinusT;
+
+
+			ResultCurve.X = oneMinusTSquared*oneMinusT*Start.X +
+				3.0*oneMinusTSquared*t*ControlPoints[0].X +
+				3.0*oneMinusT*tSquared*ControlPoints[1].X + 
+				tSquared*t*End.X;
+
+			ResultCurve.Y = oneMinusTSquared * oneMinusT * Start.Y +
+				3.0 * oneMinusTSquared * t * ControlPoints[0].Y +
+				3.0 * oneMinusT * tSquared * ControlPoints[1].Y +
+				tSquared * t * End.Y;
+
+			ResultCurve.Z = oneMinusTSquared * oneMinusT * Start.Z +
+				3.0 * oneMinusTSquared * t * ControlPoints[0].Z +
+				3.0 * oneMinusT * tSquared * ControlPoints[1].Z +
+				tSquared * t * End.Z;
+
+			CurvePoints.Add(ResultCurve);
+		}
+	}
 }
--- Source/AnotherWorldStreet/public/InitialMapGeneratorComponent.h
+++ Source/AnotherWorldStreet/public/InitialMapGeneratorComponent.h
@@ -34,8 +34,47 @@ public:
 	UFUNCTION(BlueprintCallable, BlueprintPure)
 		FMapInfoStruct GetLastResult() { return LastGenerateResult; }
 
+
+//======== FPoint 3D ===============
+public:
+	//Bezier curve 조절점의 FVector 구조체
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Control points")
+		TArray<FVector> ControlPoints;
+
+		//Bezier Curve 점 좌표들의 FVector 구조체
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Control points")
+		TArray<FVector> CurvePoints;
+
+//============ 초기모집단 생성 관련 UFUNCTION ============
+public:
+	//control point 랜덤 생성 함수
+	UFUNCTION()
+		FVector GenControlPoint();
+
+	UFUNCTION()
+		void GenBezierCurve(FVector Start, FVector End, int32 GenPointNum, int32 ControlPointNum);
+
+
 protected:	
 	//마지막 생성 결과
 	UPROPERTY(BlueprintReadWrite)
 		FMapInfoStruct LastGenerateResult;
+
+	//Gen_Control_Point함수 결과값
+	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Gen Control point")
+	//	FVector ControlPoint;
+
+	//Fvector (Start, End) Location
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Location Setting")
+		FVector StartLoc;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Location Setting")
+		FVector EndLoc;
+
+
+	//UPROPERTY(BlueprintReadOnly)
+	//	UStaticMesh StaticMeshInfo;
+
+	UPROPERTY(BlueprintReadWrite)
+		FPlatformInfoStruct PlatformInfoResult;
 };
